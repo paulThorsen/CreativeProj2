7540eccb13deb59b46d97217831c33d5c2d3d195
ShinjinE <shinjinethan@hotmail.com>
2018-10-05 05:08:53 +0000

    final files

--- js.js
+++ js.js
@@ -3,56 +3,69 @@
 $(document).ready(function() {
     var typeJoke;
 
-    $("#foodJoke").click(function(e) {
+    $("#foodjoke").click(function(e) {
         e.preventDefault();
         typeJoke = "food";
         outputJoke();
     });
 
-    $("#sportJoke").click(function(e) {
+    $("#sportjoke").click(function(e) {
         e.preventDefault();
         typeJoke = "sport";
         outputJoke();
     });
 
-    $("#historyJoke").click(function(e) {
+    $("#historyjoke").click(function(e) {
         e.preventDefault();
         typeJoke = "history";
         outputJoke();
 
     });
 
-    $("#musicJoke").click(function(e) {
+    $("#musicjoke").click(function(e) {
         e.preventDefault();
         typeJoke = "music";
         outputJoke();
 
     });
 
-    $("#travelJoke").click(function(e) {
+    $("#traveljoke").click(function(e) {
         e.preventDefault();
         typeJoke = "travel";
         outputJoke();
 
     });
 
-    $("#devJoke").click(function(e) {
+    $("#devjoke").click(function(e) {
         e.preventDefault();
         typeJoke = "dev";
         outputJoke();
     });
+    // $("#option-1").click(function(){
+    //     $("#overlay1").toggle();
+    //     $(this).toggleClass("active-button")
+    // });
 
-    function outputJoke() {
-        var myurl = "https://api.chucknorris.io/jokes/random?category=" + typeJoke;
-        console.log(myurl);
-        $.ajax({
-            url: myurl,
-            dataType: "json",
-            success: function(parsed_json) {
-                var joke = parsed_json['value'];
-                var everything = joke;
-                $("#joke").html(everything);
-            }
-        });
-    }
+    // $("#option-2").click(function(){
+    //     $("#overlay2").toggle();
+    //     $(this).toggleClass("active-button")
+    // });
+
+    // $("#option-3").click(function(){
+    //     $("#overlay3").toggle();
+    //     $(this).toggleClass("active-button")
+    // });
+        function outputJoke() {
+            var myurl = "https://api.chucknorris.io/jokes/random?category=" + typeJoke;
+            console.log(myurl);
+            $.ajax({
+                url: myurl,
+                dataType: "json",
+                success: function(parsed_json) {
+                    var joke = parsed_json['value'];
+                    var everything = joke;
+                    $("#joke").html(everything);
+                }
+            });
+        }
 });
